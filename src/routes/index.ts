faa2aff46f6b22031ac36f8b72b44c676617a24f
Sampo Antila <sampo.antila@vincit.fi>
2020-02-02 20:46:06 +0200

    better index response

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,8 @@ const router = express.Router();
 
 /* GET root */
 router.get('/', (req, res) => {
-    res.send('Tarmo ilmo - REST API usage only.');
+    res.send('<!DOCTYPE html><html lang="en"><head><meta name="description" content="Tarmo ilmo API" />'
+    + '<title>Tarmo ilmo API</title></head><body>Tarmo ilmo - REST API usage only.</body></html>');
 });
 
 router.get('/favicon.ico', (req, res) => {
